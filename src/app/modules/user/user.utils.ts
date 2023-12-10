8d012421c533cba237d8d164ecbab7afd788ca57
ixaman <iasadxaman@gmail.com>
2023-12-11 05:40:22 +0600

    bug fix: generate different student id for every new semester & year

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -14,11 +14,24 @@ const getLastStudent = async () => {
     .sort({ createdAt: -1 })
     .lean()
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined
+  return lastStudent?.id ? lastStudent.id : undefined
 }
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const initialId = (await getLastStudent()) || (0).toString()
+  let initialId = (0).toString() //0000
+
+  const lastStudentId = await getLastStudent() //2030010000
+  const lastStuentYear = lastStudentId?.substring(0, 4)
+  const lastStudentCode = lastStudentId?.substring(4, 6)
+
+  if (
+    lastStudentId &&
+    lastStuentYear === payload.year &&
+    lastStudentCode === payload.code
+  ) {
+    initialId = lastStudentId.substring(6)
+  }
+
   let incrementedId = (Number(initialId) + 1).toString().padStart(4, '0')
 
   incrementedId = `${payload.year}${payload.code}${incrementedId}`
